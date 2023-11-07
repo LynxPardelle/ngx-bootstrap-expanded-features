040df14113fa3494c1c5cde35248a0a2d163c299
Lynx Pardelle <lynxpardelle@lynxpardelle.com>
2023-11-07 01:18:25 -0600

    fix: responsividad eliminando max-width

--- projects/ngx-bootstrap-expanded-features/src/lib/ngx-bootstrap-expanded-features.service.ts
+++ projects/ngx-bootstrap-expanded-features/src/lib/ngx-bootstrap-expanded-features.service.ts
@@ -803,12 +803,15 @@ export class NgxBootstrapExpandedFeaturesService {
             this.styleConsole
           );
           this.createCSSRules(
-            `@media only screen and (min-width: ${b.value}) ${
+            `@media only screen and (min-width: ${b.value}) { html body ${b.bef}}`
+          );
+          /*
+          ${
               bpsStringed.length > 1 && i !== 0
                 ? `and (max-width: ${bpsStringed[i - 1].value})`
                 : ''
-            } { html body ${b.bef}}`
-          );
+            }
+          */
           this.createCSSRules(
             `@media only screen and (min-width: ${b.value}) ${
               bpsStringed.length > 1 && i !== 0
